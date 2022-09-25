f6efe0ba7053ced24b29c619ef6cbbe83d819a53
rebecatschiedel <rebecatcmg@gmail.com>
2022-09-24 17:40:21 -0700

    navigation functionality

---
+++ index.js
@@ -0,0 +1,15 @@
+const nav = document.querySelector(".primary-navigation");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+
+// Toggle hamburguer menu
+navToggle.addEventListener("click", function (e) {
+  const visibility = nav.getAttribute("data-visible");
+
+  if (visibility === "false") {
+    nav.setAttribute("data-visible", true);
+    navToggle.setAttribute("aria-expanded", true);
+  } else {
+    nav.setAttribute("data-visible", false);
+    navToggle.setAttribute("aria-expanded", false);
+  }
+});
